a3e9ec75c428852d1350018227e91eee592b28c9
BiswanathBD <biswanath.sarker.bd@gmail.com>
2025-10-08 16:46:26 +0600

    search functionality add and add img to json data

--- src/Components/AllApps/AllApps.jsx
+++ src/Components/AllApps/AllApps.jsx
@@ -5,6 +5,7 @@ import Loading from "../Loading/Loading";
 const AllApps = () => {
   const [allApps, setAllApps] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchItems, setSearchItems] = useState("");
 
   useEffect(() => {
     fetch("/data.json")
@@ -17,6 +18,12 @@ const AllApps = () => {
   }, []);
   if (loading) return <Loading />;
 
+  const filteredApps = searchItems
+    ? allApps.filter((app) =>
+        app.title.toLowerCase().includes(searchItems.toLowerCase())
+      )
+    : allApps;
+
   return (
     <div className="bg-gray-100 py-16 px-4">
       <div className="container mx-auto">
@@ -25,9 +32,21 @@ const AllApps = () => {
           Explore All Apps on the Market developed by us. We code for Millions
         </p>
 
+        <div className="flex justify-between items-center mb-4">
+          <p className="text-xl font-semibold">({filteredApps.length}) App Found</p>
+          <input
+            onChange={(e) => setSearchItems(e.target.value)}
+            type="search"
+            name="search"
+            id="search"
+            placeholder={` Search Apps`}
+            className="border border-gray-300 focus:outline-none focus:border-purple-400 px-4 py-1 rounded-lg"
+          />
+        </div>
+
         {/* dynamic book section */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center gap-4">
-          {allApps.map((data) => (
+          {filteredApps.map((data) => (
             <Apps key={data.id} data={data}></Apps>
           ))}
         </div>
